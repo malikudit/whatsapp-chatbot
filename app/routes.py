c1b33176bae605a1805c1d1a356051dd0ab7b4eb
Udit Malik <51827904+malikudit@users.noreply.github.com>
2020-07-28 14:59:50 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import request
-from app import app, _update_db
+from app import app, _update_db, _delete_item
 from app.models import User, Order
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -25,82 +25,77 @@ def bot():
 
     user = User.query.get(remote_number)
 
-
     # generic commands available
-    if incoming_msg == "help":
-        output_lines.append("'create account' - create a new account")
-        output_lines.append("'new order' - start your order")
-        output_lines.append("'end order' - end your order")
-
+    if incoming_msg == "help" or incoming_msg == "hello" or incoming_msg == "hi" or incoming_msg == "hey" or incoming_msg == "namaste":
+        output_lines.append("Welcome to the WhyQ chatbot! This is an automated message. Here is what this bot can do:\n")
+        output_lines.append("You can create a new account by typing 'create account' followed by your first name and pincode")
+        output_lines.append("Format: 'create account Rajesh 110019'\n")
+        output_lines.append("'new' - start your order")
+        output_lines.append("'end' - end your order")
         return _send_message(output_lines)
-
 
     # for a new user
     if not user:
-        if incoming_msg == "create account":
-            new_user = User(remote_number)
+        if incoming_msg.startswith("create account"):
+            message_parts = incoming_msg.split(" ")
+            count = len(message_parts)
+            if count > 4 or count < 4:
+                output_lines.append("Wrong format. Try again with the correct format 'create account NAME PINCODE")
+                return _send_message(output_lines)
+            name = message_parts[2].strip()
+            pincode = message_parts[3].strip()
+            new_user = User(remote_number, name, pincode)
             _update_db(new_user)
             output_lines.append(
-                f"Account successfully created for number {remote_number}!"
-            )
-            output_lines.append(
-                "To get started, text 'new order' to start a new order. Add your pincode at the end of the order after a /"
-            )
-            output_lines.append(
-                "When you're done, text 'finish order'."
-            )
-            output_lines.append(
-                "Text 'help' at any time to see available commands."
+                f"Account successfully created for {name}! Text 'new' to start your order."
             )
         else:
-            output_lines.append("Please register first by texting 'create account'")
-        
+            output_lines.append("Please register first by texting 'create account' followed by your name and pincode.'")
+            output_lines.append("Eg format: 'create account Rajesh 110019'")
         return _send_message(output_lines)
-    
+
+
     # for an existing user
-    else:
-        if incoming_msg == "create account":
-            output_lines.append(f"You have already registered {remote_number}. Text help for available options, or 'new order'.")
+    if user:
+        # confirmation message
+        if incoming_msg == "create account" or incoming_msg.startswith("create account"):
+            output_lines.append(f"Account already exists!")
+            output_lines.append("If you still want to continue, type 'confirm' to delete your current account.")
             return _send_message(output_lines)
-    
+
+        # account deletion
+        if incoming_msg == "delete":
+            output_lines.append("Account deleted! You can now create another account by typing the format 'create account NAME PINCODE'")
+            _delete_item(user)
+            return _send_message(output_lines)
+
 
     # creating a new order
-    if not user.creating_orders and incoming_msg == "new order":
-        output_lines.append("Create a new order and send it as one single text below, with the pincode at the end after a /")
+    if not user.creating_orders and incoming_msg == "new":
+        output_lines.append("Create a new order and send it below!")
         user.creating_orders = True
         _update_db(user)
         return _send_message(output_lines)
 
 
+    # checking order format
+    if user.creating_orders and incoming_msg != "end":
+        content = incoming_msg
+        new_order = Order(user.phone_number, content)
+        _update_db(new_order)
+        output_lines.append(
+            f"Listening to your order now. Add your items, and when done, type 'end'"
+        )
+        return _send_message(output_lines)
+
+
     # ending a new order
-    if user.creating_orders and incoming_msg == "finish order":
-        output_lines.append("Order created.")
+    if user.creating_orders and incoming_msg == "end":
+        output_lines.append("Order created successfully.")
         user.creating_orders = False
         _update_db(user)
         return _send_message(output_lines)
 
-
-    # checking order format
-    if user.creating_orders:
-        if "/" not in incoming_msg:
-            output_lines.append(
-                "Please include your pincode after a / at the end of your order."
-            )
-            output_lines.append("Text 'finish order' to end your order.")
-            return _send_message(output_lines)
-        else:
-            message_parts = incoming_msg.split
-            if len(message_parts) > 2:
-                output_lines.append("More than one / was sent. Please use only one / in message.")
-                return _send_message(output_lines)
-            content = message_parts[0].strip()
-            pincode = message_parts[1].strip()
-            new_order = Order(user.phone_number, content, pincode)
-            _update_db(new_order)
-            output_lines.append(
-                f"Order for pincode {pincode} created successfully."
-            )
-            return _send_message(output_lines)
 
     # error handling
     output_lines.append("Sorry, I don't understand, please try again or text 'help'.")
