81a15e0689d49a0427a0588bcefdf0d0384fa0db
Udit Malik <51827904+malikudit@users.noreply.github.com>
2020-07-28 15:00:34 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 def _update_db(obj):
     db.session.add(obj)
     db.session.commit()
     return obj
+
+def _delete_item(obj):
+    db.session.delete(obj)
+    db.session.commit()
 
 from app import routes, models
