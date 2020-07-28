f8ec966f986c591150be1257e97bc19302039e61
Udit Malik <51827904+malikudit@users.noreply.github.com>
2020-07-28 15:00:12 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,22 +1,21 @@
-from app import db
-from app import _update_db
-
+from app import db, _update_db, _delete_item
 
 class User(db.Model):
     __tablename__ = "users"
 
     phone_number = db.Column(db.Text, primary_key=True)
+    pincode = db.Column(db.Text())
     orders = db.relationship(
         "Order",
         backref = "user",
         primaryjoin = "User.phone_number == Order.user_id", 
     )
-
     creating_orders = db.Column(db.Boolean)
 
-    def __init__(self, phone_number):
+    def __init__(self, phone_number, name, pincode):
         self.phone_number = phone_number
-        self.creating_orders = True
+        self.creating_orders = False
+        self.pincode = pincode
 
 
 class Order(db.Model):
@@ -25,9 +24,7 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Text(), db.ForeignKey("users.phone_number"))
     content = db.Column(db.Text())
-    pincode = db.Column(db.Text()) 
 
-    def __init__(self, user_id, content, pincode):
+    def __init__(self, user_id, content):
         self.user_id = user_id
         self.content = content
-        self.pincode = pincode
